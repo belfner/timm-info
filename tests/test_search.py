865a7a0cd13cb8ae24718b30bbe5e3b318dacaec
belfner <belfner@belfner.com>
2023-09-08 00:04:51 -0400

    updated tool to accept multiple inputs

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,11 +6,11 @@ def test_help():
     call_timminfo(['search', '--help'])
 
 
-@pytest.mark.parametrize('name', ['""', 'xception*', 'resnet*'])
+@pytest.mark.parametrize('name', [['""'], ['xception*'], ['resnet*', 'xception', 'resnet*']])
 @pytest.mark.parametrize('pretrained', [True, False])
 @pytest.mark.parametrize('simple', [True, False])
 def test_search_for_name(name: str, pretrained: bool, simple: bool):
-    args = ['search', name]
+    args = ['search'] + name
     if pretrained:
         args.append('-p')
     if simple:
