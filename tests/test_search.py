5cf80f793135d2013d2c5db96f68ab249e4c3613
belfner <belfner@belfner.com>
2023-09-12 23:40:04 -0400

    reformatted files and updated types

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,5 +1,8 @@
-from utils import call_timminfo
+from typing import List
+
 import pytest
+
+from utils import call_timminfo
 
 
 def test_help():
@@ -9,7 +12,7 @@ def test_help():
 @pytest.mark.parametrize('name', [['""'], ['xception*'], ['resnet*', 'xception', 'resnet*']])
 @pytest.mark.parametrize('pretrained', [True, False])
 @pytest.mark.parametrize('simple', [True, False])
-def test_search_for_name(name: str, pretrained: bool, simple: bool):
+def test_search_for_name(name: List[str], pretrained: bool, simple: bool):
     args = ['search'] + name
     if pretrained:
         args.append('-p')
