272c68fcc07bc6a02cf4825227ebf65a67f4e5c3
belfner <belfner@belfner.com>
2023-09-12 23:40:04 -0400

    reformatted files and updated types

--- tests/test_info.py
+++ tests/test_info.py
@@ -1,12 +1,17 @@
-from utils import call_timminfo
+from typing import List
+
 import pytest
+
+from utils import call_timminfo
 
 
 def test_help():
     call_timminfo(['info', '--help'])
 
 
-@pytest.mark.parametrize('name',
-                         [['resnet50'], ['vgg11'], ['mobilenetv2_110d'], ['resnet50', 'vgg11', 'mobilenetv2_110d']])
-def test_get_info(name: str):
+@pytest.mark.parametrize('name', [['resnet50'],
+                                  ['vgg11'],
+                                  ['mobilenetv2_110d'],
+                                  ['resnet50', 'vgg11', 'mobilenetv2_110d']])
+def test_get_info(name: List[str]):
     call_timminfo(['info'] + name)
